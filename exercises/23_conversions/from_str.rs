163eabb180ff28fdf3379f8e2a9bb54abcb64403
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-12-22 16:53:20 +0530

    try_form-intro and from_str done

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,17 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(',');
+        let (Some(name), Some(age), None) = (split.next(), split.next(), split.next()) else {
+            return Err(ParsePersonError::BadLen);
+        };
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let age = age.parse::<u8>().map_err(ParsePersonError::ParseInt)?;
+        Ok(Person { name: name.to_string(), age })
+    }
 }
 
 fn main() {
--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -28,14 +28,28 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (r, g, b) = tuple;
+        Ok(Color {
+            red: r.try_into().map_err(|_| IntoColorError::IntConversion)?,
+            green: g.try_into().map_err(|_| IntoColorError::IntConversion)?,
+            blue: b.try_into().map_err(|_| IntoColorError::IntConversion)?,
+        })
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let [r, g, b] = arr;
+        Ok(Color {
+            red: r.try_into().map_err(|_| IntoColorError::IntConversion)?,
+            green: g.try_into().map_err(|_| IntoColorError::IntConversion)?,
+            blue: b.try_into().map_err(|_| IntoColorError::IntConversion)?,
+        })
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +57,18 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let [r, g, b] = [slice[0], slice[1], slice[2]];
+        Ok(Color {
+            red: r.try_into().map_err(|_| IntoColorError::IntConversion)?,
+            green: g.try_into().map_err(|_| IntoColorError::IntConversion)?,
+            blue: b.try_into().map_err(|_| IntoColorError::IntConversion)?,
+        })
+        
+    }
 }
 
 fn main() {
